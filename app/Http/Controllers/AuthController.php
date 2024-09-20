ff9bff5f10b48e68c9d9da0bf11889974bfe7d60
DwipashaDjango <dwipasha336@gmail.com>
2024-09-20 21:19:22 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,10 +25,18 @@ class AuthController extends Controller
 
         $data = json_decode($response->getContent(), true);
 
-        if (isset($data) && $data["status"] == true) {
-            Session::put("authorization", $data["data"]["token"]["plainTextToken"]);
-            Session::put("role", $data["data"]["user"]["roles"][0]["name"]);
+        if ($data && isset($data['status']) && $data['status'] === true) {
+            if (isset($data['data']['token']['plainTextToken']) && isset($data['data']['user']['roles'][0]['name'])) {
+                Session::put('authorization', $data['data']['token']['plainTextToken']);
+                Session::put('role', $data['data']['user']['roles'][0]['name']);
+            } else {
+                return response()->json(['error' => 'Token atau role tidak ditemukan'], 400);
+            }
+        } else {
+            return response()->json(['error' => 'Login gagal, periksa kredensial Anda'], 401);
         }
+
+        return $response;
 
         return $response;
     }
